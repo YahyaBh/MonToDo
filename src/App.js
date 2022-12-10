d3516d5338a48e5a226487d11c4e8cb54a2dd2ea
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2022-12-11 00:05:39 +0100

    m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import './App.css';
-import { FaClipboardList } from 'react-icons/fa'
+import { FaClipboardList } from 'react-icons/fa';
+import { BsCardChecklist } from 'react-icons/bs';
+import { BiListPlus } from 'react-icons/bi';
+import { AiFillStar, AiFillHome } from 'react-icons/ai';
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+
 
 function App() {
+
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
+
   return (
     <>
-      <nav>
-        <h2>MonToDo <FaClipboardList /></h2>
-      </nav>
+      <header>
+
+        <div className="wrapper">
+          <motion.div
+            className="container"
+          >
+            <motion.div
+              className="item"
+              style={{
+                scaleY: scrollYProgress
+              }}
+            />
+          </motion.div>
+        </div>
+
+        <nav>
+          <ul>
+            <h2>MonToDo</h2>
+            <li><a href="#home">HOME <AiFillHome /></a></li>
+            <li><a href="#news">TASKS <BsCardChecklist /></a></li>
+            <li><a href="#contact">STARED  <AiFillStar /></a></li>
+            <li><a href="#contact">CREATE  <BiListPlus /></a></li>
+          </ul>
+        </nav>
+
+        <div className='task-box'>
+
+        </div>
+
+      </header>
     </>
   );
 }
