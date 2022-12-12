0707618a7d1e8c298821e1db93eb29ea771d0708
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2022-12-12 20:10:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import { FaClipboardList } from 'react-icons/fa';
 import { BsCardChecklist } from 'react-icons/bs';
 import { BiListPlus } from 'react-icons/bi';
@@ -11,6 +12,32 @@ function App() {
 
   const { scrollYProgress } = useViewportScroll();
   const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
+
+  const [items, setItems] = useState(['hello' , 'weclome']);
+  const [taskInput, setTaskInput] = useState([]);
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('items'));
+    if (items) {
+      setItems(items);
+      console.log(items);
+    }
+  }, []);
+
+
+  function SaveTask() {
+    if (taskInput.length !== 0) {
+      setItems(taskInput);
+      localStorage.setItem('items', JSON.stringify(items));
+      setTaskInput('');
+    }
+    // else {
+    //   alert('Can not be empty !')
+    // }
+
+  }
+
+
 
   return (
     <>
@@ -41,7 +68,7 @@ function App() {
 
         <div className='header-container'>
           <div className='header-texts'>
-            <h1>Make Your Time <b style={{color : 'rgb(var(--orange-color))'}}>Organized</b></h1>
+            <h1>Make Your Time <b style={{ color: 'rgb(var(--orange-color))' }}>Organized</b></h1>
             <p>Make each task with it specific time , to make your life a lot easier <br />and make your time much meanful</p>
           </div>
           <div className='task-box'></div>
@@ -51,7 +78,19 @@ function App() {
 
 
       <section id='tasks' name="tasks">
-          <h2>YOUR TASKS</h2>
+        <h2>YOUR TASKS</h2>
+        {items.length === 0 ? 'Data Not Saved' :
+          items.map((item, index) => (
+            <h3 key={index}>{item}</h3>
+          ))}
+
+
+
+
+        <input name='task' onChange={e => setTaskInput(e.target.value)} />
+
+        <button onClick={SaveTask()}>Check</button>
+
       </section>
     </>
   );
